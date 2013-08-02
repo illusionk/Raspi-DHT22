d08c0659ddaf4af65e62e8fba8def339325cfae9
illusionk <illusionk@me.com>
2013-08-02 19:48:02 +0800

    fix tab and layout on history.xml

--- script/history_script.py
+++ script/history_script.py
@@ -35,5 +35,5 @@ file = open('/home/pi/www/DHT22/xml/history.xml', 'w')
 file.write("<CATALOG>")
 for cell in array:
 	file.write("\n\t<RECORD>\n\t\t<DATE>%s</DATE>\n\t\t<TEMP>%s</TEMP>\n\t\t<HUD>%s</HUD>\n\t</RECORD>" % (cell[0], cell[1], cell[2]))
-file.write("</CATALOG>")
+file.write("\n</CATALOG>")
 file.close()
